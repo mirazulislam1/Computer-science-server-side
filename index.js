c51660af750bb5aeaf09096b34b720daba32e404
unknown <you@example.com>
2022-10-26 11:04:29 +0600

    node added

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json')
 
 // make api
 
 app.get('/', (req, res) =>{
     res.send('computer science api');
 });
+
+
+
+
+app.get('/courses', (req, res) =>{
+    res.send(categories)
+})
 
 app.listen(port, () => {
     console.log('computer science running on port', port);
